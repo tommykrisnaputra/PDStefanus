0e04115e60446967ef8c085cfeae435e7101320f
TOMMY-PC\Hakuzen PC <tommykpwork@gmail.com>
2023-02-22 10:57:39 +0700

    Improved Laravel Blade formatting

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav data-role="Header" class="home-navbar">
+    <a href="/users" class="home-navlink">
+        <img alt="image" src="images/logo1-600h.png" loading="lazy" class="home-image" />
+    </a>
+    <h1 class="home-logo">
+        <p>PD OMPKK St. Stefanus</p>
+        <p>@yield('title')</p>
+    </h1>
+    <div class="home-right-side">
+        <div class="home-links-container">
+            <a href="/" class="home-navlink">Home</a>
+            <span class="home-text">Info Paroki</span>
+            <a href="/users" class="solid-button-container">
+                <button class="solid-button-button button Button">
+                    <span>Login</span>
+                </button>
+            </a>
+        </div>
+    </div>
+</nav>

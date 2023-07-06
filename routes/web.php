fe5a402487439f572db2106728caaadcdfb09e06
tommykpwork@gmail.com <phantasia888>
2023-07-06 10:34:36 +0700

    Updated login routes

--- routes/web.php
+++ routes/web.php
@@ -25,20 +25,18 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::get('/success', 'HomeController@success')->name('success');
 
     /**
+     * Login Routes
+     */
+    Route::get('/login', 'LoginController@show')->name('login.show');
+    Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    /**
      * Register Routes
      */
     Route::get('/register', 'RegisterController@show')->name('register.show');
     Route::post('/register', 'RegisterController@register')->name('register.perform');
     Route::get('/attendance', 'AttendanceController@show')->name('attendance.show');
     Route::post('/attendance', 'AttendanceController@register')->name('attendance.perform');
-
-    Route::group(['middleware' => ['guest']], function () {
-        /**
-         * Login Routes
-         */
-        Route::get('/login', 'LoginController@show')->name('login.show');
-        Route::post('/login', 'LoginController@login')->name('login.perform');
-    });
 
     Route::group(['middleware' => ['auth']], function () {
         /**

44cb27d28590699210a87cc104df6c59ab6166e4
tommykpwork@gmail.com <phantasia888>
2023-07-13 11:41:06 +0700

    Fixed login on mobile

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -55,6 +55,7 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
+        $request->session()->regenerate();
         return redirect('/');
     }
 }

8829f0669d1bbbc86866be6b1cea9ddc4be44ef5
tommykpwork@gmail.com <phantasia888>
2024-03-14 10:50:34 +0700

    Updated page formatting for team attendance

--- app/Http/Controllers/TeamAttendanceController.php
+++ app/Http/Controllers/TeamAttendanceController.php
@@ -12,7 +12,7 @@ class TeamAttendanceController extends Controller
     public function index ( $id )
         {
         $events     = TeamEvents::find ( $id );
-        $attendance = TeamAttendance::where ( 'team_event_id', $id )->join ( 'users', 'users.id', 'team_attendances.user_id' )->select ( 'team_attendances.*', 'users.full_name as name' )->orderBy('name', 'asc')->get ();
+        $attendance = TeamAttendance::where ( 'team_event_id', $id )->join ( 'users', 'users.id', 'team_attendances.user_id' )->select ( 'team_attendances.*', 'users.full_name as name' )->orderBy ( 'active', 'asc' )->orderBy ( 'name', 'asc' )->get ();
         return view ( 'team-attendance.form', [ 'events' => $events, 'attendance' => $attendance ] );
         }
 
--- resources/views/team-attendance/form.blade.php
+++ resources/views/team-attendance/form.blade.php
@@ -7,8 +7,8 @@
 @section('css')
 	@parent
 	<link href="{{ asset('css/events/index.css') }}" rel="stylesheet">
-	<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 @endsection
 
 @section('navbar')
@@ -18,37 +18,33 @@
 @section('content')
 	<div class="home-main" id="main-section">
 		<div class="container">
-			<div class="event">
-				<div class="col col-3">{{ $events->title }} - </div>
-				<div class="col col-3">{{ Carbon\Carbon::parse($events->date)->format('d M Y') ?? null }}</div>
+			<div class="row justify-content-md-center event">
+				<div class="col col-10">{{ $events->title }}</div>
+				<div class="col col-2">{{ Carbon\Carbon::parse($events->date)->format('d M Y') ?? null }}</div>
 			</div>
 
 			@if ($events->description)
-				<div class="event">
-					<div class="col col-3" data-label="Deskripsi">{{ $events->description ?? null }}</div>
+				<div class="row justify-content-md-center event">
+					<div class="col col-12" data-label="Deskripsi">{{ $events->description ?? null }}</div>
 				</div>
 			@endif
 
-			<ul class="responsive-table">
-				<li class="table-header">
-					<div class="col col-3">Nama</div>
-					<div class="col col-3">Kehadiran</div>
-				</li>
+			<div class="row">
 				@foreach ($attendance as $key => $data)
-					<li class="table-row">
-						<div class="col col-3" data-label="Nama">{{ $data->name ?? null }}</div>
-						<div class="col col-3" data-label="Action">
+					<div class="col col-lg-4 col-md-6 col-sm-12 attendance">
+						<div class="col col-6" data-label="Nama">{{ $data->name ?? null }}</div>
+						<div class="col col-6" data-label="Action">
 							<a class="solid-button-container" href="{{ url("team-attendance/update/$data->id") }}">
 								@if ($data->active == '1')
-									<button class="solid-button-button-active Button">Hadir</button>
+									<button class="solid-button-button-active Button attendance-font">Hadir</button>
 								@else
-									<button class="solid-button-button button Button">Belum Hadir</button>
+									<button class="solid-button-button-nonactive button Button attendance-font mt-8">Belum Hadir</button>
 								@endif
 							</a>
 						</div>
-					</li>
+					</div>
 				@endforeach
-			</ul>
+			</div>
 		</div>
 	</div>
 @endsection

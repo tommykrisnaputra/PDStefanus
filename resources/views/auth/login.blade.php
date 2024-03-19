872b185dcafe9c6602d33619d893d011f2718063
tommykpwork@gmail.com <phantasia888>
2024-03-19 09:47:10 +0700

    Added floating whatsapp button in homepage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,77 @@
 @extends('master')
 
 @section('title')
-    Login
+	Login
 @endsection
 
 @section('css')
-    @parent
-    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+	@parent
+	<link href="{{ asset('css/auth/login.css') }}" rel="stylesheet">
 @endsection
 
 @section('navbar')
-    @parent
+	@parent
 @endsection
 
 @section('content')
-    <div id="main-section" class="home-main">
-        @if ($errors->any())
-            <div class="alert-toast">
-                <div class="alert-toast-content">
-                    <div class="message">
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        @endif
-        <div class="container">
-            <form method="POST" action="/login">
-                @csrf 
-                <div class="row">
-                    <div class="col-25">
-                        <label for="email">Email atau Nomor HP</label>
-                    </div>
-                    <div class="col-75">
-                        <input type="text" id="email" name="email" value="{{ old('email') }}"
-                            placeholder="Masukan Email atau nomor HP" class="{{ $errors->has('email') ? 'form-error' : '' }}">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="password">Password</label>
-                    </div>
-                    <div class="col-75 password-container">
-                        <input type="password" id="password" name="password" value="{{ old('password') }}"
-                            placeholder="Masukan Password" class="{{ $errors->has('password') ? 'form-error' : '' }}">
-                        <i class="far fa-eye" id="eye"></i>
-                    </div>
-                </div>
-                <div class="row submit-button-container">
-                    <input type="submit" value="Submit" class="submit-button">
-                </div>
-            </form>
-        </div>
-    </div>
+	<div class="home-main" id="main-section">
+		@if ($errors->any())
+			<div class="alert-toast">
+				<div class="alert-toast-content">
+					<div class="message">
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</div>
+				</div>
+			</div>
+		@endif
+		<div class="container">
+			<form method="POST" action="/login">
+				@csrf
+				<div class="row">
+					<div class="col-25">
+						<label for="email">Email atau Nomor HP</label>
+					</div>
+					<div class="col-75">
+						<input class="{{ $errors->has('email') ? 'form-error' : '' }}" id="email" name="email" type="text"
+							value="{{ old('email') }}" placeholder="Masukan Email atau nomor HP">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<label for="password">Password</label>
+					</div>
+					<div class="col-75 password-container">
+						<input class="{{ $errors->has('password') ? 'form-error' : '' }}" id="password" name="password" type="password"
+							value="{{ old('password') }}" placeholder="Masukan Password">
+						<i class="far fa-eye" id="eye"></i>
+					</div>
+				</div>
+				<div class="row submit-button-container">
+					<input class="submit-button" type="submit" value="Submit">
+				</div>
+			</form>
+		</div>
+	</div>
 @endsection
 
 @section('js')
-    <script>
-        const passwordInput = document.querySelector("#password")
-        const eye = document.querySelector("#eye")
-        eye.addEventListener("click", function() {
-            this.classList.toggle("fa-eye-slash")
-            const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
-            passwordInput.setAttribute("type", type)
-        })
+	<script>
+		const passwordInput = document.querySelector("#password")
+		const eye = document.querySelector("#eye")
+		eye.addEventListener("click", function() {
+			this.classList.toggle("fa-eye-slash")
+			const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+			passwordInput.setAttribute("type", type)
+		})
 
-        const passwordConfirmationInput = document.querySelector("#password_confirmation")
-        const eyeConfirmation = document.querySelector("#eye-confirmation")
-        eyeConfirmation.addEventListener("click", function() {
-            this.classList.toggle("fa-eye-slash")
-            const type = passwordConfirmationInput.getAttribute("type") === "password" ? "text" : "password"
-            passwordConfirmationInput.setAttribute("type", type)
-        })
-    </script>
+		const passwordConfirmationInput = document.querySelector("#password_confirmation")
+		const eyeConfirmation = document.querySelector("#eye-confirmation")
+		eyeConfirmation.addEventListener("click", function() {
+			this.classList.toggle("fa-eye-slash")
+			const type = passwordConfirmationInput.getAttribute("type") === "password" ? "text" : "password"
+			passwordConfirmationInput.setAttribute("type", type)
+		})
+	</script>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -65,6 +65,11 @@
 				@endforeach
 			</div>
 		</div>
+		<a class="float"
+			href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
+			target="_blank">
+			<i class="fa fa-whatsapp my-float"></i>
+		</a>
 	</div>
 	@include('footer')
 @endsection
